e0cce6cb8944cc9236161a6a8641df3508d07795
mgunawardhana <mrgunawardhana27368@gmail.com>
2025-02-02 21:30:38 +0530

    project cleanup

--- src/app/main/live-aquaria/sample-component/GeneralAdvertisementRoot.tsx
+++ src/app/main/live-aquaria/sample-component/GeneralAdvertisementRoot.tsx
@@ -1,7 +1,6 @@
 import { FuseRouteConfigsType } from '@fuse/utils/FuseUtils';
-import GeneralAdvertisementConfig from './root-component/GeneralAdvertisementConfig';
-import GeneralAdvertisementPublishConfig from './general-advertisement-publish/GeneralAdvertisementPublishConfig';
 
-const GeneralAdvertisementRoot: FuseRouteConfigsType = [GeneralAdvertisementConfig, GeneralAdvertisementPublishConfig];
+
+const GeneralAdvertisementRoot: FuseRouteConfigsType = [];
 
 export default GeneralAdvertisementRoot;
